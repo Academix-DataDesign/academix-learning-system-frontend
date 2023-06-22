052c07e4b37073f871f079ae3b183217ef691a16
Jovan Milanovic <jovanmilanovic999@gmail.com>
2023-06-22 19:54:29 +0200

    finish input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react"
+import Input from "./components/UI/Input"
 
 function App() {
   return (
     <Fragment>
      <h1>Hello world!</h1>
+     <Input classes={['idk','idk']} type="text" placeholder={"idk"}/>
     </Fragment>
   )
 }
---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,36 @@
+
+interface BoxProps {
+  classes: string[];
+  type: string;
+  required?: boolean; 
+  placeholder: string;
+  options?: string[]; 
+}
+
+const Input: React.FC<BoxProps> = ({ classes, type, required, placeholder, options }) => {
+  if (type === "select" && options && options.length > 0) {
+    return (
+      <div className={classes.join(" ")}>
+        <div className="rectangle-wrapper">
+          <select required={required} placeholder={placeholder}>
+            {options.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.join(" ")}>
+      <div className="rectangle-wrapper">
+        <input type={type} required={required} placeholder={placeholder} />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
