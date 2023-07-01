2064dd6fc5fa05770ad70ab047086fed1b522c82
Nikola <perisicnikola37@gmail.com>
2023-07-02 00:11:36 +0200

    Test auto deployment script

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Fragment } from "react"
-import Input from "./components/UI/Input/Input"
-import Jodza from "./Jodza"
+import { Fragment } from "react";
+import Input from "./components/UI/Input/Input";
+import Jodza from "./Jodza";
 
 function App() {
   return (
     <Fragment>
-     <h1>Hello world!</h1>
-     <Input classes={['idk','idk']} type="text" placeholder={"idk"}/>
-     <Jodza/>
+      <h1>Hello world! Checking auto deployment feature!</h1>
+      <Input classes={["idk", "idk"]} type="text" placeholder={"idk"} />
+      <Jodza />
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
