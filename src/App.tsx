93e2552b7c85e08308e5ddef7a3d77c4eacaee34
Jodzav2 <105559220+Jodzirv2@users.noreply.github.com>
2023-07-07 21:30:23 +0200

    Revert to light mode

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Vocabulary from "./domain/pages/Vocabulary/Vocabulary";
 import UserRegister from "./domain/pages/Register/UserRegister/UserRegister";
 import InstructorRegister from "./domain/pages/Register/InstructorRegister/InstructorRegister";
 import Login from "./domain/pages/Login/Login";
+import Home from "./domain/pages/Home/Home";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register-user" element={<UserRegister />} />
           <Route path="/register-instructor" element={<InstructorRegister />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/vocabulary" element={<Vocabulary />} />
         </Routes>
       </Router>
