3ea5ff2df5bafdfa2f38c5df67b7d374b09885a4
Nikola <perisicnikola37@gmail.com>
2023-07-09 18:05:32 +0200

    Fix workflow

--- src/Show.tsx
+++ src/Show.tsx
@@ -4,14 +4,18 @@ import { Button } from "./domain/UI/Button/Button";
 const Show = () => {
   return (
     <>
-      <h1>Hello there team of Academix!</h1>
-      <h2>We will build something awesome!</h2>
-      <Input variant="defaul" placeholder="Email" />
+      {/* <h1>Hello there team of Academix!</h1> */}
+      {/* <h2>We will build something awesome!</h2> */}
+      {/* <Input variant="default" placeholder="Email" /> */}
       <br />
-      <Button variant="login2">Log In</Button>
+      {/* <Button variant="login2">Log In</Button>
       <Button variant="login">Log In</Button>
       <Button variant="register">Register</Button>
-      <Button variant="register2">Register</Button>
+      <Button variant="register2">Register</Button> */}
+      <a style={{ color: "#000" }} href="/register-learner">
+        {" "}
+        ovdje klikni da bi vidio završeno
+      </a>
     </>
   );
 };
