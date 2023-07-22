be70acf01946e81d8bb33d1f8c7a40230405f0e0
Jodzav2 <105559220+Jodzirv2@users.noreply.github.com>
2023-07-22 23:05:18 +0200

    Make header more responsive

--- src/Show.tsx
+++ src/Show.tsx
@@ -1,20 +1,10 @@
-import Input from "./domain/UI/Input/Input";
-import { Button } from "./domain/UI/Button/Button";
-import Slider from "./domain/UI/Slider/Slider";
+import Navbar from "./domain/components/Navbar/Navbar";
 
 const Show = () => {
   return (
     <>
-      <h1>Hello there team of Academix!</h1>
-      <br />
-      <br />
-      <Slider />
-      {/* <Input variant="defaul" placeholder="Email" /> */}
-      <br />
-      <Button variant="login2">Log In</Button>
-      <Button variant="login">Log In</Button>
-      <Button variant="register">Register</Button>
-      <Button variant="register2">Register</Button>
+      
+      <Navbar />
     </>
   );
 };
--- src/domain/components/Navbar/Navbar.tsx
+++ src/domain/components/Navbar/Navbar.tsx
@@ -1,28 +1,50 @@
-import './Navbar.scss'
+import Logo from "../../../assets/Academix-Logo.png";
+import { Button } from "../../UI/Button/Button";
+
+import classes from "./Navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <header className="header">
-      <div
-        className="avatar"
-        style={{
-          backgroundImage:
-            "url(https://anima-uploads.s3.amazonaws.com/projects/649474d8048a6087ab20c40b/releases/6495c491a1b0005e5700e644/img/ikonica@2x.png)",
-        }}
-      >
-        <div className="ellipse-4" />
+    <header>
+      <div className={classes["left-side"]}>
+        <span className={classes.logo}>
+          <img src={Logo} />
+          <span>Academix</span>
+        </span>
+        <ul className={classes.navbar}>
+          <li>
+            <span className="active">Home</span>
+          </li>
+          <li>
+            <span>About</span>
+          </li>
+          <li>
+            <span>Courses</span>
+          </li>
+          <li>
+            <span>Categories</span>
+          </li>
+        </ul>
       </div>
-      <div className="navbar">
-        <div className="text-wrapper-37">Courses</div>
-        <div className="text-wrapper-38">Categories</div>
-        <div className="text-wrapper-39">About</div>
-        <div className="text-wrapper-40">Home</div>
+      <div className={classes.main}>
+        <span>
+          <Button
+            variant={"login"}
+            style={{ width: "200px", height: "65px", fontSize: "24px" }}
+          >
+            Login
+          </Button>
+        </span>
+        <span>
+          <Button
+            variant={"register"}
+            style={{ width: "200px", height: "65px", fontSize: "24px" }}
+          >
+            Register
+          </Button>
+        </span>
+        <div className="bx bx-menu" id="menu-icon"></div>
       </div>
-      <img
-        className="logo-2"
-        alt="Logo"
-        src="https://anima-uploads.s3.amazonaws.com/projects/649474d8048a6087ab20c40b/releases/6495c491a1b0005e5700e644/img/logo-1@2x.png"
-      />
     </header>
   );
 };
